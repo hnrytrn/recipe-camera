5ce7f5c73a36c34dde8ca2e508bf2bec8ada4ec7
Henry Tran <22henrytran@gmail.com>
2016-08-28 14:47:08 -0400

    initial commit

---
+++ app/src/main/java/com/example/henrytran/recipecamera/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.henrytran.recipecamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
