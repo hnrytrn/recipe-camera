66511ae59e2c5bceda0743e624c339f3042c1dcc
Henry Tran <22henrytran@gmail.com>
2016-08-28 15:53:47 -0400

    add clarifai's food recognition

--- app/src/main/java/com/example/henrytran/recipecamera/MainActivity.java
+++ app/src/main/java/com/example/henrytran/recipecamera/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.henrytran.recipecamera;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.clarifai.api.ClarifaiClient;
+import com.clarifai.api.RecognitionRequest;
+import com.clarifai.api.RecognitionResult;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final ClarifaiClient clarifaiClient = new ClarifaiClient(BuildConfig.CLARIFAI_CLIENT_ID,
+            BuildConfig.CLARIFAI_CLIENT_SECRET);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +40,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
+                try {
+                    // get the image captured as a bitmap
+                    InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    //scale down image to improve performance
+                    bitmap = Bitmap.createScaledBitmap(bitmap, 320,
+                            320 * bitmap.getHeight() / bitmap.getWidth(), true);
 
+                    // run image recognition on a background thread
+                    new AsyncTask<Bitmap, Void, RecognitionResult>() {
+                        @Override
+                        protected RecognitionResult doInBackground(Bitmap... bitmaps) {
+                            // Compress image as a JPEG
+                            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                            bitmaps[0].compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
+                            byte[] jpeg = outputStream.toByteArray();
+
+                            //send jpeg to clarifai client and return result
+                            return clarifaiClient.recognize(new RecognitionRequest(jpeg).setModel("food-item-v1.0")).get(0);
+                        }
+
+                        @Override
+                        protected void onPostExecute (RecognitionResult result) {
+
+                        }
+                    }.execute(bitmap);
+                } catch (FileNotFoundException e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                }
+            } else if (requestCode == RESULT_CANCELED) {
+                // user cancelled the image capture
+            } else {
+                // capture failed
             }
         }
     }
