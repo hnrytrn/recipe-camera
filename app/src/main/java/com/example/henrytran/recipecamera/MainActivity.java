cbc6e6da561245f1d62d071c434889317c5b1a77
Henry Tran <22henrytran@gmail.com>
2016-08-28 15:01:22 -0400

    add image capture intent

--- app/src/main/java/com/example/henrytran/recipecamera/MainActivity.java
+++ app/src/main/java/com/example/henrytran/recipecamera/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.henrytran.recipecamera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int CAMERA_REQUEST_CODE = 100;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // intent for using the camera to take a picture
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+            }
+        }
     }
 }
